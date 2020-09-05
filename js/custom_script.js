49cbf2cfbd1b29073d7c1f636f3b82a3bcf6e799
Chiranjeevi karthik <karthikchiru12@gmail.com>
2020-09-05 19:16:52 +0530

    Added a starting alert. And Removed vertical slides.

---
+++ js/custom_script.js
@@ -0,0 +1,4 @@
+function opening_alert()
+{
+    alert("Welcome to Tarka Sangraha Slides...\n\n If you are using desktop use left and right arrow keys\nto change the slides.\n\n If you are using mobile you can swipe to change the slides.");
+}
