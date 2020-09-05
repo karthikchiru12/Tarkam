67ae7907311fada8eb3b102f779bb022c8fa2411
Chiranjeevi karthik <karthikchiru12@gmail.com>
2020-09-05 19:23:16 +0530

    Removed the extra space in the alert message.

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,4 +1,4 @@
 function opening_alert()
 {
-    alert("Welcome to Tarka Sangraha Slides...\n\n If you are using desktop use left and right arrow keys\nto change the slides.\n\n If you are using mobile you can swipe to change the slides.");
+    alert("Welcome to Tarka Sangraha Slides...\n\nIf you are using desktop use left and right arrow keys to change the slides.\n\nIf you are using mobile you can swipe right or left to change the slides.");
 }
