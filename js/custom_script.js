c323ceb1912b83807096a5e1c77dc905094734d8
Chiranjeevi karthik <karthikchiru12@gmail.com>
2020-09-05 20:11:26 +0530

    Modified alert message and added a demo meaning vertical slide

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,4 +1,4 @@
 function opening_alert()
 {
-    alert("Welcome to Tarka Sangraha Slides...\n\nIf you are using desktop use left and right arrow keys to change the slides.\n\nIf you are using mobile you can swipe right or left to change the slides.");
+    alert("Welcome to Tarka Sangraha Slides...\n\nIf you are using desktop use left and right arrow keys to change the slides.\nIf you are using mobile you can swipe right or left to change the slides.\nYou can view the meaning by swiping down(in mobile) and in desktop you can click down arrow button.");
 }
